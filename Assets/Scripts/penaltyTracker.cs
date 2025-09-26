3d6d9d440ef2ddec256c9f7f35cd89e7467746a3
tikzyy <145325630+tikzyy@users.noreply.github.com>
2025-09-26 11:18:49 +0200

    updated penaltyTracker.cs

--- Assets/Scripts/penaltyTracker.cs
+++ Assets/Scripts/penaltyTracker.cs
@@ -1,14 +1,46 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public static class penaltyTracker
 {
-    void Start()
+    private static int _penaltyFine = 0;
+    private static int _penaltyPoints = 0;
+    private static bool _isSuspended = false;
+    private static readonly int suspensionThreshold = 3;
+    
+    
+    public static int PenaltyFine => _penaltyFine;
+    public static int PenaltyPoints => _penaltyPoints;
+    public static bool IsSuspended => _isSuspended;
+    
+    public static void AddPenalty(int fineAmount)
     {
-        DontDestroyOnLoad(this.gameObject);
+        _penaltyFine += fineAmount;
+        CheckSuspensionStatus();
+    }
+    
+    public static void AddPenalty(int fineAmount, int penaltyAmount)
+    {
+        _penaltyFine += fineAmount;
+        _penaltyPoints += penaltyAmount;
+        CheckSuspensionStatus();
+    }
+    
+    private static void CheckSuspensionStatus()
+    {
+        if (_penaltyPoints >= suspensionThreshold) {
+            _isSuspended = true;
+        }
     }
 
-    void Update()
+    private static void ResetPenalties()
     {
-        
+        _penaltyFine = 0;
+        _penaltyPoints = 0;
+        _isSuspended = false;
+    }
+    
+    public static string GetPenaltySummary()
+    {
+        return $"Fine: ${_penaltyFine}, Points: {_penaltyPoints}, Suspended: {_isSuspended}";
     }
 }
